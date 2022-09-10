f630de32586b26b923bf6c32a96c633bd6ecb0d8
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-09-10 21:01:01 +0530

    Time: 52 ms (96.40%), Space: 13.8 MB (97.52%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.py
+++ 929-unique-email-addresses/929-unique-email-addresses.py
@@ -4,8 +4,7 @@ class Solution:
         count = 0
         for email in emails:
             local,domain = email.split("@")
-            local = local.replace(".","").split("+")[0]
-            email = local+"@"+domain
+            email = local.replace(".","").split("+")[0] + "@" + domain
             if email not in output:
                 output.add(email)
                 count += 1
