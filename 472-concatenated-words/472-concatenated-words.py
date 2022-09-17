2c6be783936eb4500161cb43fd99ec719197400e
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-09-17 13:53:17 +0530

    Time: 2399 ms (6.53%), Space: 27.6 MB (55.62%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -1,19 +1,19 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         mem = set(words)
-        mem2 = {}
+        # mem2 = {}
         output = []
         def dfs(word):
-            if word in mem2:
-                return mem2[word]
+            # if word in mem2:
+            #     return mem2[word]
             for i in range(1, len(word)):
                 pre = word[:i]
                 suf = word[i:]
                 
                 if (pre in mem and suf in mem) or (pre in mem and dfs(suf)) or (suf in mem and dfs(pre)):
-                    mem2[word] = True
+                    # mem2[word] = True
                     return True
-            mem2[word] = False
+            # mem2[word] = False
             return False
         
         for w in words:
