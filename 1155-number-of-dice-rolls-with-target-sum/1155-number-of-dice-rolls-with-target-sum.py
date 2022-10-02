96cae64a26cfa4a06b6300cf2855970efd114575
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-02 11:43:35 +0530

    Time: 1738 ms (13.18%), Space: 16.6 MB (47.70%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,31 +1,21 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        mem  = [[-1] * target for i in range(n)]
-        # print(mem)
-        def helper(dice,face,t):
-            if t < 0 :
+        dp = [[-1] * target for i in range(n)]
+        def check(d,f,t):
+            if t<0:
                 return 0
-            if dice == 0 and t == 0:
+            if d == 0 and t == 0:
                 return 1
-            if dice == 0 and t != 0:
+            if (d==0 and t != 0) or (d!=0 and t==0) :
                 return 0
-            if dice != 0 and t == 0:
-                return 0
-            
-            
-            if mem[dice-1][t-1] != -1:
-                return mem[dice-1][t-1]
-            
+            if dp[d-1][t-1] != -1:
+                return dp[d-1][t-1]
             
             output = 0
-            for i in range(1,face+1):
-                if t-i <0:
+            for i in range(1,f+1):
+                if t-i<0:
                     break
-                output += helper(dice-1,face,t-i)
-            mem[dice-1][t-1] = output
-            # print(dice,t,output)
-            return mem[dice-1][t-1]
-        out = helper(n,k,target)
-        # print(mem)
-        return out%1000000007
-        
+                output += check(d-1,f,t-i)
+            dp[d-1][t-1] = output
+            return output
+        return check(n,k,target)%1000000007
