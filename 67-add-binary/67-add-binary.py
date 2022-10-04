a46e71874e11a95ce35e2a477d1abf19c71e0657
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-04 12:49:04 +0530

    Time: 62 ms (41.13%), Space: 13.9 MB (25.59%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        # a,b = a[::-1], b[::-1]
+        # carry = 0
+        # output = []
+        # i = 0
+        # for i,j in zip(a,b):
+        #     if a == b == 1:
+        #         output.append("0")
+        #     elif a == b == 
+        return bin(int(a,2) + int(b,2))[2:]
+            
+            
+        
+        
