134c07b02cc4c0e7888c958d8ccba5fb45bf6a97
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-04 14:47:46 +0530

    Time: 816 ms (84.42%), Space: 32.8 MB (87.95%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -1,21 +1,15 @@
 from itertools import chain
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        output = 0
-        if len(nums2) %2 != 0:
-            for i in nums1:
-                output ^= i
-                # print(output)
-        if len(nums1) %2 != 0:
-            for i in nums2:
-                output ^= i
-                # print(output)
-        return output
-        # x = y = 0
-        # for a in nums1:
-        #     x ^= a
-        #     print(x)
-        # for b in nums2:
-        #     y ^= b
-        #     print(y)
-        # return (len(nums1) % 2 * y) ^ (len(nums2) % 2 * x)
+        # output = 0
+        # if len(nums2) %2 != 0:
+        #     for i in nums1:
+        #         output ^= i
+        #         # print(output)
+        # if len(nums1) %2 != 0:
+        #     for i in nums2:
+        #         output ^= i
+        #         # print(output)
+        # return output
+        # print(reduce(xor, nums2))
+        return ((len(nums1) %2) * reduce(xor, nums2)) ^ ((len(nums2) %2) * reduce(xor, nums1))
