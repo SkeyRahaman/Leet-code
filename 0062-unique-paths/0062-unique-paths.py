05b18fc062dd952188ab3093b83b1ca2425a779b
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-16 21:25:01 +0530

    Time: 74 ms (7.66%), Space: 14.2 MB (6.30%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @lru_cache(None)
+        def helper(i,j):
+            if i==0 and j == 0:
+                return 1
+            if i<0 or j <0:
+                return 0
+            top = helper(i-1,j)
+            left = helper(i,j-1)
+            return top+left
+        return helper(m-1,n-1)
