c18b9b22ce918469e468d34829cd364add7b88f7
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-16 22:05:20 +0530

    Time: 64 ms (26.46%), Space: 13.9 MB (74.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,12 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @lru_cache
-        def helper(i,j):
-            if i==0 and j == 0:
-                return 1
-            if i<0 or j <0:
-                return 0
-            top = helper(i-1,j)
-            left = helper(i,j-1)
-            return top+left
-        return helper(m-1,n-1)
+        mem = [0] * n
+        mem[0] = 1
+        for _ in range(m):
+            for i in range(1,n):
+                mem[i] = mem[i] + mem[i-1]
+        return mem[-1]
