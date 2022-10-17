7267bf89d6770789129cc264af943b31aa5dc46b
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-17 22:08:10 +0530

    Time: 117 ms (63.21%), Space: 15 MB (62.94%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,6 +1,24 @@
 from functools import lru_cache
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1]
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j],dp[j+1])
+        return dp[0]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         h = len(triangle)
         @lru_cache
         def helper(i,d,h):
