2edc11c16e557a509d37f9b8659d712be07d2d89
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-18 22:17:39 +0530

    Time: 9388 ms (5.09%), Space: 14.7 MB (92.90%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,28 +1,33 @@
+
+from copy import deepcopy
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         N = len(grid[0])
         M = len(grid)
         # @cache
-        dp = [[[-1] * N for _ in range(N)] for _ in range(M)]
+        front = [[-1] * N for _ in range(N)]
+        cur = [[-1] * N for _ in range(N)]
         for i in range(M-1,-1,-1):
             for j1 in range(N):
                 for j2 in range(N):
                     if i == M-1:
                         if j1 == j2:
-                            dp[i][j1][j2] = grid[i][j1]
+                            cur[j1][j2] = grid[i][j1]
                         else:
-                            dp[i][j1][j2] = grid[i][j1] + grid[i][j2]
+                            cur[j1][j2] = grid[i][j1] + grid[i][j2]
                     else:
                         maxi = float('-inf')
                         for d1 in [-1,0,1]:
                             for d2 in [-1,0,1]:
                                 if 0<=(j1+d1)<N and 0<=(j2+d2)<N :
                                     if j1 != j2:
-                                        maxi = max(maxi,grid[i][j1] + grid[i][j2] + dp[i+1][j1+d1][j2+d2])
+                                        maxi = max(maxi,grid[i][j1] + grid[i][j2] + front[j1+d1][j2+d2])
                                     else:
-                                        maxi = max(maxi,grid[i][j1] + dp[i+1][j1+d1][j2+d2])
-                        dp[i][j1][j2] = maxi
-        return dp[0][0][N-1]
+                                        maxi = max(maxi,grid[i][j1] + front[j1+d1][j2+d2])
+                        cur[j1][j2] = maxi
+            front = deepcopy(cur)
+            # cur = [[-1] * N for _ in range(N)]
+        return front[0][N-1]
         
         
         
