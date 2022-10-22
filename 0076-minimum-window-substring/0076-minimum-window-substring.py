c237d4cdf23d1d1f1188512d1d2729ff9a03fd8b
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-22 13:36:00 +0530

    Time: 240 ms (48.72%), Space: 14.6 MB (83.90%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        output = [float('inf'), 0, 0] # length , start , end
         need = Counter(t)
-        have = dict()
+        need_n = len(need)
+        have = defaultdict(int)
         have_n = 0
         first = 0
-        need_n = len(need)
+        output = [0,0,float('inf')]
         for i,c in enumerate(s):
             if c in need:
-                # print(c)
-                if c in have:
-                    have[c] += 1
-                else:
-                    have[c] = 1
-                if need[c] == have[c]:
+                have[c] += 1
+                if have[c] == need[c]:
                     have_n += 1
                 if have_n == need_n:
-                    pp = first
-                    for k in range(pp,i+1):
-                        # print(output,c,have,need,have_n,first,i)
-                        if s[k] in need:
-                            have[s[k]] -= 1
-                            if need[s[k]] > have[s[k]]:
+                    for j in range(first,i+1):
+                        if s[j] in need:
+                            have[s[j]] -= 1
+                            if have[s[j]] < need[s[j]]:
                                 have_n -= 1
                                 break
-                            else:
-                                first += 1
-                        else:
-                            first += 1   
-                    length = i-first+1
-                    if length < output[0]:
-                        output = [length,first,i+1]
+                        first += 1
+                    if output[2] > (i+1)-first:
+                        output = [first,i+1,(i+1)-first]
                     first += 1
-        return s[output[1]:output[2]]
+        # print()
+        return s[output[0]:output[1]]
+                
+        
+        
