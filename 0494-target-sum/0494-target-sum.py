b49e7c0a989e046c9b0180a57b0f025976abfcc0
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-23 23:40:37 +0530

    Time: 419 ms (72.39%), Space: 19.1 MB (55.32%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        S = sum(nums)
+        dp = [[-1] * (2*S+5) for i in nums]
+        def helper(i,m):
+            if i == 0:
+                output = 0
+                if m + nums[0] == target:
+                    output += 1
+                if m - nums[0] == target:
+                    output += 1
+                return output
+            if dp[i][m] != -1:
+                return dp[i][m]
+            dp[i][m] =  helper(i-1,m+nums[i]) + helper(i-1,m-nums[i])
+            return dp[i][m]
+        
+        x = helper(len(nums)-1,0)
+        # print(dp)
+        return x
+        
