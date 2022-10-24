c7b554fa85c9acb8c41be470ab0823da42827a16
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-24 15:52:02 +0530

    Time: 130 ms (99.45%), Space: 35 MB (25.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,16 +1,19 @@
 class Solution:
-    def change(self, amount: int, nums: List[int]) -> int:
-        @cache
+    def change(self, t: int, nums: List[int]) -> int:
+        dp = [[-1] * (t+1) for _ in nums]
         def helper(i,t):
             if i == 0:
                 if t%nums[0]==0:
                     return 1
                 else:
                     return 0
+            if dp[i][t] != -1:
+                return dp[i][t]
             notTake = helper(i-1,t)
             take = 0
             if nums[i] <= t:
                 take = helper(i,t-nums[i])
-            return take + notTake
-        return helper(len(nums)-1,amount)
+            dp[i][t] =  take + notTake
+            return dp[i][t]
+        return helper(len(nums)-1,t)
             
