e6312a239c8d163170ac8a8f81ca82f0d737d519
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-25 22:40:10 +0530

    Time: 822 ms (65.29%), Space: 21.8 MB (87.22%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -0,0 +1,39 @@
+class Solution:
+    def shortestCommonSupersequence(self, s1: str, s2: str) -> str:
+        dp = [[0] * (len(s2)+1) for i in range(len(s1)+1)]
+        for i in range(len(s1)-1,-1,-1):
+            for j in range(len(s2)-1,-1,-1):
+                if s1[i] == s2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        i = 0
+        j = 0
+        output = []
+        while i<len(s1) and j <len(s2):
+            if s1[i] == s2[j]:
+                output.append(s1[i])
+                i += 1
+                j += 1
+            else:
+                if dp[i+1][j] > dp[i][j+1]:
+                    i += 1
+                else:
+                    j+=1
+        # print(output)
+        j,k=0,0
+        o=[]
+        for i in output:
+            while j<len(s1) and s1[j] != i:
+                o.append(s1[j])
+                j += 1
+            while k<len(s2) and s2[k] != i:
+                o.append(s2[k])
+                k+=1
+            o.append(i)
+            j+=1
+            k+=1
+        o.append(s1[j:])
+        o.append(s2[k:])
+        return "".join(o)
+            
