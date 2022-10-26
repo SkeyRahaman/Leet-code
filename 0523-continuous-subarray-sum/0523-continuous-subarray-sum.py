9a615511fd3432f6b79ba01b8bc05dd08cec4aaa
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 09:37:33 +0530

    Time: 1080 ms (88.24%), Space: 33.7 MB (21.71%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        seen, cur = {0: -1}, 0
+        for i, a in enumerate(nums):
+            cur = (cur + a) % abs(k) if k else cur + a
+            if i - seen.setdefault(cur, i) > 1: return True
+        return False
+    
+    
+        mem = {0:-1}
+        s = 0
+        for j,i in enumerate(nums):
+            s = s+i if k==0 else (s+i)%k
+            if s in mem:
+                if j-mem[s] >= 2:
+                    return True
+            mem[s] = i
+        return False
+                
+            
+        
