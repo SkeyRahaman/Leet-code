a44231a007ab3411a8fde76c51ff637c885e337c
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 11:54:55 +0530

    Time: 782 ms (74.81%), Space: 70.5 MB (62.47%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -18,17 +18,17 @@ class Solution:
         
         
         
-        dp= [[-1] * len(t) for _ in s]
-        def helper(i,j):
-            if j<0:
-                return 1
-            if i<0:
-                return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            if s[i] == t[j]:
-                dp[i][j] = helper(i-1,j-1) + helper(i-1,j)
-            else:
-                dp[i][j] = helper(i-1,j)
-            return dp[i][j]
-        return helper(len(s)-1,len(t)-1)
+        # dp= [[-1] * len(t) for _ in s]
+        # def helper(i,j):
+        #     if j<0:
+        #         return 1
+        #     if i<0:
+        #         return 0
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     if s[i] == t[j]:
+        #         dp[i][j] = helper(i-1,j-1) + helper(i-1,j)
+        #     else:
+        #         dp[i][j] = helper(i-1,j)
+        #     return dp[i][j]
+        # return helper(len(s)-1,len(t)-1)
