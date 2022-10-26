80195ff5a48aafd30dbb1ba30cd9785480821523
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 20:09:25 +0530

    Time: 2115 ms (20.44%), Space: 14.1 MB (83.61%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,5 +1,24 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        last = [False] * (len(p)+1)
+        cur  = [False] * (len(p)+1)
+        last[0] = True
+        for j in range(1,len(p)+1):
+            last[j] = "*"*(j) == p[:j]
+        for i in range(1,len(s)+1):
+            for j in range(1,len(p)+1):
+                if s[i-1] == p[j-1] or p[j-1] == "?":
+                    cur[j] = last[j-1]
+                elif p[j-1] == "*":
+                    if cur[j-1]:cur[j] = True
+                    else:cur[j] = last[j]
+                else:
+                    cur[j] =  False
+            last = [z for z in cur]
+        return last[len(p)]
+        
+        
+        
         dp = [[False] * (len(p)+1) for _ in range(len(s)+1)]
         dp[0][0] = True
         for j in range(1,len(p)+1):
