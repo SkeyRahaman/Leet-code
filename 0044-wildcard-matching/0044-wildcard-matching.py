3911680116b1a48117326deb4579bd83a525acde
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 20:06:15 +0530

    Time: 756 ms (76.10%), Space: 22.4 MB (70.17%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,5 +1,22 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p)+1) for _ in range(len(s)+1)]
+        dp[0][0] = True
+        for j in range(1,len(p)+1):
+            dp[0][j] = "*"*(j) == p[:j]
+        for i in range(1,len(s)+1):
+            for j in range(1,len(p)+1):
+                if s[i-1] == p[j-1] or p[j-1] == "?":
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == "*":
+                    if dp[i][j-1]:dp[i][j] = True
+                    else:dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] =  False
+        return dp[len(s)][len(p)]
+        
+        
+        
         dp = [[-1] * len(p) for _ in range(len(s))]
         def helper(i,j):
             #base case
@@ -10,6 +27,8 @@ class Solution:
             if i == -1:
                 return "*"*(j+1) == p[:j+1]
             if dp[i][j] != -1:return dp[i][j]
+            
+            
             if s[i] == p[j] or p[j] == "?":
                 dp[i][j] = helper(i-1,j-1)
             elif p[j] == "*":
@@ -17,6 +36,8 @@ class Solution:
                 else:dp[i][j] = helper(i-1,j)
             else:
                 dp[i][j] =  False
+                
+                
             return dp[i][j]
         return helper(len(s)-1,len(p)-1)
         
