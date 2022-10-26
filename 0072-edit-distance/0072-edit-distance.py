7422ddd369e4e719c0f9c19a2ecdc59e9f427be9
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 15:53:36 +0530

    Time: 210 ms (73.41%), Space: 17.1 MB (70.91%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,17 +1,20 @@
 class Solution:
     def minDistance(self, s1: str, s2: str) -> int:
-        @cache
+        dp = [[-1] * (len(s2)+1) for i in range(len(s1)+1)]
         def helper(i,j):
             if j == -1:
                 return i +1
             if i == -1:
                 return j + 1
-            
+            if dp[i][j] != -1:
+                return dp[i][j]
             if s1[i] == s2[j]:
-                return  helper(i-1,j-1)
-            insert = helper(i,j-1)
-            delete = helper(i-1,j)
-            replace = helper(i-1,j-1)
-            return 1 + min(insert,delete,replace)
+                dp[i][j] =   helper(i-1,j-1)
+            else:
+                insert = helper(i,j-1)
+                delete = helper(i-1,j)
+                replace = helper(i-1,j-1)
+                dp[i][j] =  1 + min(insert,delete,replace)
+            return dp[i][j]
         return helper(len(s1)-1,len(s2)-1)
         
