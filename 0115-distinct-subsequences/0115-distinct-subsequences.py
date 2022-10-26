cc7f99f05eca918db9b1a17062c366a0db15dd7b
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 11:46:56 +0530

    Time: 1681 ms (35.52%), Space: 31.1 MB (75.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,13 +1,16 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        @cache
+        dp= [[-1] * len(t) for _ in s]
         def helper(i,j):
             if j<0:
                 return 1
             if i<0:
                 return 0
+            if dp[i][j] != -1:
+                return dp[i][j]
             if s[i] == t[j]:
-                return helper(i-1,j-1) + helper(i-1,j)
+                dp[i][j] = helper(i-1,j-1) + helper(i-1,j)
             else:
-                return helper(i-1,j)
+                dp[i][j] = helper(i-1,j)
+            return dp[i][j]
         return helper(len(s)-1,len(t)-1)
