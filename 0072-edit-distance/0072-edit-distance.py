3292d50f1778b707a082290162e17e71e1fc4325
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 16:29:33 +0530

    Time: 354 ms (46.01%), Space: 13.8 MB (99.94%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,17 +1,19 @@
 class Solution:
     def minDistance(self, s1: str, s2: str) -> int:
-        dp = [[0] * (len(s2)+1) for _ in range(len(s1)+1)]
-        for i in range(1,len(s1)+1):
-            dp[i][0] = i
+        last = [0] * (len(s2)+1)
+        crr = [0] * (len(s2)+1)
         for j in range(len(s2)+1):
-            dp[0][j] = j
+            last[j] = j
+            
         for i in range(1,len(s1)+1):
+            crr[0] = i
             for j in range(1,len(s2)+1):
                 if s1[i-1] == s2[j-1]:
-                    dp[i][j] =   dp[i-1][j-1]
+                    crr[j] =   last[j-1]
                 else:
-                    dp[i][j] =  1 + min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])
-        return dp[len(s1)][len(s2)]
+                    crr[j] =  1 + min(crr[j-1],last[j],last[j-1])
+            last = [z for z in crr]
+        return last[len(s2)]
 
         
         
