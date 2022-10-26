a4763149bf1c85a047af22c999e80b99a4a3392a
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-26 19:57:28 +0530

    Time: 1305 ms (52.81%), Space: 24.7 MB (39.13%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,6 +1,6 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        @lru_cache(None)
+        dp = [[-1] * len(p) for _ in range(len(s))]
         def helper(i,j):
             #base case
             if i == -1 and j==-1:
@@ -9,13 +9,14 @@ class Solution:
                 return False
             if i == -1:
                 return "*"*(j+1) == p[:j+1]
-            
+            if dp[i][j] != -1:return dp[i][j]
             if s[i] == p[j] or p[j] == "?":
-                return helper(i-1,j-1)
+                dp[i][j] = helper(i-1,j-1)
             elif p[j] == "*":
-                if helper(i,j-1):return True
-                return helper(i-1,j)
+                if helper(i,j-1):dp[i][j] = True
+                else:dp[i][j] = helper(i-1,j)
             else:
-                return False
+                dp[i][j] =  False
+            return dp[i][j]
         return helper(len(s)-1,len(p)-1)
         
