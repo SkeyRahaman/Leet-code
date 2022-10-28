64378b78febfe0b01288fb792e728aded9949664
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-28 09:18:04 +0530

    Time: 255 ms (27.76%), Space: 17.9 MB (55.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,6 +2,6 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         mem = defaultdict(list)
         for i in strs:
-            mem[tuple(sorted(i))].append(i)
+            mem["".join(sorted(i))].append(i)
         return mem.values()
         
