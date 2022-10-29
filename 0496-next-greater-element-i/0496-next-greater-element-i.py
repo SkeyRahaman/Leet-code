48667ecadee744f799f25507c4aea4ff2e1bf344
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-29 23:30:14 +0530

    Time: 115 ms (36.41%), Space: 14.1 MB (56.69%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,13 +2,13 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         mem = []
         nums3 = defaultdict(int)
-        for i in range(len(nums2)-1,-1,-1):
-            while mem and nums2[i] > mem[-1]:
+        for i in nums2[::-1]:
+            while mem and i > mem[-1]:
                 mem.pop()
             if mem:
-                nums3[nums2[i]] = mem[-1] 
+                nums3[i] = mem[-1] 
             else:
-                nums3[nums2[i]] = -1
-            mem.append(nums2[i])
+                nums3[i] = -1
+            mem.append(i)
         return [nums3[i] for i in nums1]
         
