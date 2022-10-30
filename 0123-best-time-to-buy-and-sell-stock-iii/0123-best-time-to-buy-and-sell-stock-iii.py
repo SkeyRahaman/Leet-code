0ea114b5afd760e49cd80d506c2042e8207fb410
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-30 23:39:19 +0530

    Time: 9000 ms (5.00%), Space: 28.9 MB (33.97%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,6 +1,26 @@
 class Solution:
     def maxProfit(self, v: List[int]) -> int:
         n = len(v)
+        last = [[0,0,0],[0,0,0]] 
+        cur = [[0,0,0],[0,0,0]] 
+        for i in range(n-1,-1,-1):
+            cur[1][2] =  max(-v[i] + last[0][1],  last[1][2])
+            cur[1][1] =  max(-v[i] + last[0][0],  last[1][1])
+            cur[0][0] = max(+v[i] + last[1][0],  last[0][0])
+            cur[0][1] = max(+v[i] + last[1][1],  last[0][1])
+            cur[0][2] = max(+v[i] + last[1][2],  last[0][2])
+            last = copy.deepcopy(cur)
+        return last[1][2]
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        n = len(v)
         dp = [[[-1,-1,-1],[-1,-1,-1]] for i in v]
         def f(i,b,t):
             if i == n:return 0
