c591198511e05991ed5617ee6e1996af7530b878
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-31 20:29:01 +0530

    Time: 522 ms (20.30%), Space: 89.5 MB (5.01%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,15 +1,16 @@
 class Solution:
     def maxProfit(self, k: int, v: List[int]) -> int:
         n = len(v)
+        # dp = [-1,-1] *
         @cache
         def f(i,k,b):
-            if i==n:return 0
-            if b and k>0:
+            if i==n or (b and k<=0) :return 0
+            
+            
+            if b:
                 return max(-v[i] + f(i+1,k-1,0),f(i+1,k,1))
-            if not b:
-                return max( v[i] +f(i+1,k,1),f(i+1,k,0) )
             else:
-                return 0
+                return max( v[i] +f(i+1,k,1),f(i+1,k,0) )
         return f(0,k,1)
             
         
