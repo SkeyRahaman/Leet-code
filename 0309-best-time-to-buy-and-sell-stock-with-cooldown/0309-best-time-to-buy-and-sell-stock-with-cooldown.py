870e0e3bd8b8f303767c0c893d718af9ef74a3c9
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-10-31 21:24:08 +0530

    Time: 174 ms (7.26%), Space: 14.1 MB (81.76%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,6 +1,28 @@
 class Solution:
     def maxProfit(self, v: List[int]) -> int:
         n = len(v)
+        last = [[0 for z in range(2)] for _  in range(2)]
+        cur = [[0 for z in range(2)] for _  in range(2)] 
+        for i in range(n-1,-1,-1):
+            for b in range(2):
+                for c in range(2):
+                    if c == 0:
+                        if b:
+                            cur[b][c] =  max(-v[i] + last[0][0], last[1][0])
+                        else:
+                            cur[b][c] =  max(v[i] + last[1][1], last[0][0])
+                    else:
+                        cur[b][c] =  last[b][c-1]
+            last = copy.deepcopy(cur)
+        return last[1][0]
+
+        
+        
+        
+        
+        
+        
+        n = len(v)
         @cache
         def f(i,b,c):
             if i == n:
