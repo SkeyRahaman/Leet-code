2eb05bd7895cb8aa568d0da26a2c269a3cb36543
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-11-02 10:49:03 +0530

    Time: 24 ms (99.11%), Space: 14 MB (37.43%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        bank = {i for i in bank}
+        if end not in bank:return -1
+        gens = [i for i in 'ACGT']
+        visited = set(start)
+        q1 = [[i for i in start]]
+        q2 = []
+        output = 0
+        while q1:
+            q2 = copy.deepcopy(q1)
+            q1 = []
+            output += 1
+            for gen in q2:
+                for i in range(8):
+                    for j in range(4):
+                        x = [z for z in gen]
+                        if x[i] != gens[j]:
+                            x[i] = gens[j]
+                            y = "".join(x)
+                            if y == end:
+                                return output
+                            if y not in visited and y in bank:
+                                q1.append(y)
+        return -1
+                                
+                            
