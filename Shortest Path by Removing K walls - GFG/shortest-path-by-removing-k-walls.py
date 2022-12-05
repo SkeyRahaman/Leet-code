667c073631518efa8d184c30075de77b82a81b38
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-12-05 12:48:54 +0530

    Added solution - LeetHub

--- Shortest Path by Removing K walls - GFG/shortest-path-by-removing-k-walls.py
+++ Shortest Path by Removing K walls - GFG/shortest-path-by-removing-k-walls.py
@@ -2,31 +2,32 @@
 from collections import deque
 class Solution:
     def shotestPath(self, grid, n, m, k):
-        # code here 
-        m, n = len(grid), len(grid[0])
-        
         if k >= m + n - 2:                     #early return
             return m + n - 2
+        visited = set()
+        q = deque([[0,0,k,0]])
+        visited = set([(0,0,k)])
         
-        queue = deque([(0, 0, k, 0)])
-        visited = set([(0, 0, k)])
         
-        while queue:
-            x, y, k, steps = queue.popleft()
-            
-            if x == m-1 and y == n-1: 
-                return steps
-            
-            for i, j in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
-                if 0 <= i < m and 0 <= j < n:
-                    if grid[i][j] == 1 and (i, j, k-1) not in visited and k > 0:    #using a ticket to go to a point
-                        queue.append((i, j, k-1, steps+1))
-                        visited.add((i, j, k-1))
-                    elif grid[i][j] == 0 and (i, j, k) not in visited:              #without using ticket 
-                        queue.append((i, j, k, steps+1))
-                        visited.add((i, j, k))
-        
+        while len(q) != 0:
+            x,y,wall,step = q.popleft()
+            if x == n-1 and y == m-1:return step
+            for i,j in [[-1,0],[0,1],[1,0],[0,-1]]:
+                new_x = x+i
+                new_y = y+j
+                if 0<=new_x<n and 0<=new_y<m:
+                    if grid[new_x][new_y] == 1:
+                        if wall>0 and (new_x,new_y,wall-1) not in visited:
+                            visited.add((new_x,new_y,wall-1))
+                            q.append([new_x,new_y,wall-1,step+1])
+                    elif (new_x,new_y,wall) not in visited:
+                        visited.add((new_x,new_y,wall))
+                        q.append([new_x,new_y,wall,step+1])
         return -1
+        
+            
+        # code here 
+
     
     
 
