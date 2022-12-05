ef0ee0f0cb2ededed69bd39a680b10942a392cc0
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2022-12-05 20:15:48 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.py
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.py
@@ -1,10 +1,56 @@
 #User function Template for python3
 import heapq
+class DisjointSet:
+    def __init__(self, length: int):
+        self.size = [1] * length
+        self.par = [i for i in range(length)]
+
+    def find_u_par(self, node):
+        if self.par[node] != node:
+            self.par[node] = self.find_u_par(self.par[node])
+        return self.par[node]
+
+    def union_by_size(self, a, b):
+        par_a = self.find_u_par(a)
+        par_b = self.find_u_par(b)
+        if self.size[par_a] > self.size[par_b]:
+            self.size[par_a] += self.size[par_b]
+            self.par[par_b] = par_a
+        else:
+            self.size[par_b] += self.size[par_a]
+            self.par[par_a] = par_b
+
+    def check_same_set(self, a, b):
+        return self.find_u_par(a) == self.find_u_par(b)
 class Solution:
     
     #Function to find sum of weights of edges of the Minimum Spanning Tree.
     def spanningTree(self, V, adj):
-        #code here
+        edg = []
+        for i in range(V):
+            for j,w in adj[i]:
+                edg.append([w,i,j])
+        edg.sort(key = lambda x :x[0])
+        # print(edg)
+        output = 0
+        dj = DisjointSet(V+1)
+        for w,i,j in edg:
+            if not dj.check_same_set(i,j):
+                # print(i,j)
+                dj.union_by_size(i,j)
+                output += w
+        return output
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # Prime's Algorithm
         heap = []
         heapq.heappush(heap, (0,0,-1))
         visited = set()
