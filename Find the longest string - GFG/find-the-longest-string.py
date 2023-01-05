b83ac266b0c69b10317e052b5dfcae88d17e0ccf
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2023-01-05 19:30:14 +0530

    Added solution - LeetHub

--- Find the longest string - GFG/find-the-longest-string.py
+++ Find the longest string - GFG/find-the-longest-string.py
@@ -1,9 +1,52 @@
 #User function Template for python3
+from collections import defaultdict
 
 
 class Solution():
     def longestString(self, arr, n):
+        #Tire
+        class Tirenode:
+            def __init__(self,char="",end=False):
+                self.char = char
+                self.end = end
+                self.children = {}
+        class Tire:
+            def __init__(self):
+                self.head = Tirenode()
+            def add(self,word):
+                ptr = self.head
+                for i in range(len(word)):
+                    if word[i] not in ptr.children:
+                        ptr.children[word[i]] = Tirenode(char=word[i])
+                    ptr = ptr.children[word[i]]
+                    if i == len(word)-1:
+                        ptr.end = True
+            def check(self,word):
+                ptr = self.head
+                for i in word:
+                    ptr = ptr.children[i]
+                    if not ptr.end:return False
+                return True
+        T = Tire()
+        for i in arr:
+            T.add(i)
+        output = ""
+        for i in arr:
+            if len(i) >= len(output):
+                if T.check(i):
+                    if len(i) > len(output):
+                        output = i
+                    else:
+                        output = min(output,i)
+        return output
+                
+        
+        
+        
+        
+        
         #your code goes here
+        # Brute Force
         dp = set(arr)
         def check(s):
             nonlocal dp
