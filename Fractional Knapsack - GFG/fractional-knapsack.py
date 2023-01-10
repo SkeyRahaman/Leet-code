82f79b237a04101cbf233c678d3b0aaf1c170291
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2023-01-10 20:08:39 +0530

    Added solution - LeetHub

---
+++ Fractional Knapsack - GFG/fractional-knapsack.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to get the maximum total value in the knapsack.
+    def fractionalknapsack(self, W,Items,n):
+        bag = W
+        val = 0
+        for item in sorted(Items,key = lambda x:x.value/x.weight, reverse = True):
+            v = item.value
+            w = item.weight
+            if w>bag:
+                val += v/w*bag
+                break
+            else:
+                bag -= w
+                val += v
+        return val
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+class Item:
+    def __init__(self,val,w):
+        self.value = val
+        self.weight = w
+        
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,W = map(int,input().strip().split())
+        info = list(map(int,input().strip().split()))
+        Items = [Item(0,0) for i in range(n)]
+        for i in range(n):
+            Items[i].value = info[2*i]
+            Items[i].weight = info[2*i+1]
+            
+        ob=Solution()
+        print("%.2f" %ob.fractionalknapsack(W,Items,n))
+
+# } Driver Code Ends
