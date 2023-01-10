2fc60c87753680c714a37d1884e8f185590a1111
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2023-01-10 12:50:58 +0530

    Added solution - LeetHub

---
+++ N meetings in one room - GFG/n-meetings-in-one-room.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the maximum number of meetings that can
+    #be performed in a meeting room.
+    def maximumMeetings(self,n,start,end):
+        # code here
+        mlast = -1
+        output = 0
+        for i,j in sorted(zip(start,end),key = lambda x:x[1]):
+            if i > mlast:
+                output += 1
+                mlast = j
+        return output
+                
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        start = list(map(int,input().strip().split()))
+        end = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.maximumMeetings(n,start,end))
+# } Driver Code Ends
