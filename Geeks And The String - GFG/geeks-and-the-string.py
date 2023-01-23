6dd62f7255e106e6356f46f63a055bbbc621837d
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2023-01-23 12:12:03 +0530

    Added solution - LeetHub

---
+++ Geeks And The String - GFG/geeks-and-the-string.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to remove pair of duplicates from given string using Stack.
+    def removePair(self,s):
+        stack = []
+        for i in s:
+            if stack and stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack) if len(stack) != 0 else -1
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        obj = Solution()
+        print(obj.removePair(str(input())))
+# } Driver Code Ends
