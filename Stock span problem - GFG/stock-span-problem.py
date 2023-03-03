766f2981a5e4c93b4703350d07b63906805dd61b
MD SHAKIB MONDAL <52163557+SkeyRahaman@users.noreply.github.com>
2023-03-03 21:44:44 +0530

    Added solution - LeetHub

---
+++ Stock span problem - GFG/stock-span-problem.py
@@ -0,0 +1,51 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to calculate the span of stockâ€™s price for all n days.
+    def calculateSpan(self,a,n):
+        #code here
+        output = []
+        stack = [[float('inf'),0]]  # number, number less than this imidately before this.
+        for i in a:
+            count = 1
+            while stack[-1][0] <= i:
+                count += stack[-1][1]
+                stack.pop()
+            stack.append([i,count])
+            output.append(count)
+        return output
+        
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nikhil Kumar Singh
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        obj = Solution()
+        ans = obj.calculateSpan(a, n);
+        for i in range(n):
+            print(ans[i],end=" ")
+        print()            
+# } Driver Code Ends
